3cac9bb10ca76d984d053e693eb62e327913a68f
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 00:41:40 +0800

    Create liham.php

---
+++ TFA4/liham.php
@@ -0,0 +1,11 @@
+<div class = "story-card">
+    <h1><b> Ang Liham</b></h1>
+    <p>
+        Ilang beses itinanggi ni Anya na hindi niya gusto si Ben. Ilang beses na pinatunayan ni Ben ang sarili niya kay Anya ngunit bigo itong makuha ang atensyon ng dalaga.
+        Hanggang sa hindi inaasahan, isang araw ay nakatanggap na lamang si Ben ng liham mula kay Anya. 
+        Nakasaad ang rason ng dalaga kung bakit hindi niya agad napagbigyan ang binata ngunit siyang tuwa ng binata ng mabasa ang huling nakasulat sa liham ni Anya,
+        "Kung handa ka pa ring maghintay, handa na rin akong sumubok muli".
+    </p>
+</div>  
+
+<?php include 'footer.php'; ?>
