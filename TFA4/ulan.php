b8ba1f9532106c96afe16f65f5f9744df086abdc
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 00:41:42 +0800

    Create ulan.php

---
+++ TFA4/ulan.php
@@ -0,0 +1,10 @@
+<div class = "card">
+    <h1><b> Ulan </b></h1>
+
+    <!-- image obtain from Pinterest -->
+    <img src="ulan.jpg" alt="Image" width="500px" height="300px">
+    <p>
+        Nang bumuhos ang ulan, sabay na sumilong ang magkaibigan sa iisang payong. 
+        Tahimik nilang tinatahak ang kahabaan ng Espana, ngunit sa bawat pagpatak ng ulan, may damdaming unti-unting sumisibol na hindi nila mabanggit.
+    </p>
+</div>  
