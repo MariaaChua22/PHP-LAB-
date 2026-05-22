c9fb576d9f4320105231f440b22572310476b252
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-22 16:25:06 +0800

    Create tsa1#2.php

---
+++ TFA1/tsa1#2.php
@@ -0,0 +1,105 @@
+<html>  
+
+<head>  
+
+    <title>Multiplication Table</title>  
+
+    <style>  
+
+        body {  
+
+            font-family: Arial;  
+
+            background-color: #c5b9cd;  
+
+        }  
+
+  
+
+        h2 {  
+
+            text-align: center;  
+
+            color: #000000;  
+
+        }  
+
+  
+
+        table {  
+
+            margin: auto;  
+
+        }  
+
+  
+
+        td {  
+
+            width: 40px;  
+
+            height: 40px;  
+
+            text-align: center;  
+
+            border: 1px solid black;  
+
+            font-weight: bold; 
+
+        }  
+
+    </style>  
+
+</head>  
+
+  
+
+<body>  
+
+    <h2>Multiplication Table</h2>  
+
+  
+
+    <table>  
+
+        <?php  
+
+        for ($row = 0; $row <= 10; $row++) {  
+
+            echo "<tr>";  
+
+  
+
+            for ($column = 0; $column <= 10; $column++) {  
+
+                $answer = $row * $column;  
+
+  
+
+                if (($row + $column) % 2 == 0) {  
+
+                    $color = "#dec2cb";  
+
+                } else {  
+
+                    $color = "#abb1cf";  
+
+                }  
+
+  
+
+                echo "<td style='background-color:$color;'>$answer</td>";  
+
+            }  
+
+  
+
+            echo "</tr>";  
+
+        }  
+        ?>  
+    </table> 
+</body>  
+</html> 
+
+ 
