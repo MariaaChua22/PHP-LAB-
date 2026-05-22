ca287248cb1ed36557560b633e00be483e122554
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 00:41:48 +0800

    Create magkaibigan.php

---
+++ TFA4/magkaibigan.php
@@ -0,0 +1,13 @@
+<div class = "card">
+    <h1><b> Ang Magkaibigan </b></h1>
+
+    <!-- image obtain from Pinterest -->
+    <img src="magkaibigan.jpg" alt="Avatar" style = "width:100%">
+    <div class="container">
+    <p>
+        Isang araw, dumalo ang dalaga sa liga ng kanyang kaibigan. Hindi man ito nakaplano ay pinilit niyang magpunta dahil nangako siyang susuportahan ang binata. 
+        Hindi niya akalain na ang simpleng pagdalo na iyon ang magiging simula ng isang pagkakaibigang unti-unting lumalalim.
+        Matapos ang laro ay napagpasyahan nilang umuwi. Nauunang naglakad ang dalaga at pinagmamasdan lang siya ng binata na siyang nasa likuran ng dalaga. Tuluyang umaasa ang binata na sana'y mapansin siya ng dalaga. 
+        Ngunit lingin sa kaalaman ng binata, unti unti na rin palang nahuhulog ang dalaga sakanya.
+    </p>
+</div>  
