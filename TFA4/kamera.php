40d45781c90c22da5617c22df0cd308b842d489b
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 00:41:50 +0800

    Create kamera.php

---
+++ TFA4/kamera.php
@@ -0,0 +1,11 @@
+<div class = "card">
+    <h1><b> Kamera </b></h1>
+    <p>
+        Sa ilalim ng dapithapon, hawak ni Kenji ang kayang kamera. Hindi niya layunin ang perpektong kuha, kundi ang mahuli ang sandaling hindi na mauulit.
+        Sa kabilang banda, nakatayo si Hana, nakatingin sa langit at pinagmamasdan ang araw na unti unting lumulubog.
+        Binalot sila ng katahimikan at tanging pag-click ng kamera ni Kenji ang maririnig. 
+        Sa huling kuha bago tuluyang lumubog ang araw, napangiti si Hana at iyon ang kinunan ni Kenji.
+        Sa bawat litrato, may kwento at damdaming hindi maipapahayag ng salita, ngunit silang dalawa lamang ang tunay na nakakaalam.
+</div>  
+
+<?php include 'footer.php'; ?>
