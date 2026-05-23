2865548558d927d48dc992848c5417a362683d93
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 08:56:29 +0800

    Update obra.php

--- TFA4/obra.php
+++ TFA4/obra.php
@@ -1,11 +1,13 @@
-<div class = "card">
-    <h1><b> Obra </b></h1>
+<?php require ('header.php') ?>
 
-    <!-- image obtain from Pinterest -->
-    <img src="obra.jpg" alt="Image" width="500px" height="300px">
-    <p>
+<div class="div">
+    <h1><b> Obra </b></h1>
+    <hr>
+    <p><h2>
         Isang taong labis na mangarap. Isang obra maestrong guhit ang tinitingala ng karamihan.
         Maraming tao ang nagaakalang ganoon din ang kanyang buhay, ngunit sa likod ng kanyang mga obra ay isang taong may mga pangarap at mga kwento na hindi nakikita ng iba.
         Ang mga larawan na siyang nagsisilbing salamin sa buhay na matagal na niyang pinapangarap.
-    </p>
-</div>  
+    </p></h2>
+</div>
+
+<?php include ('footer.php') ?>
