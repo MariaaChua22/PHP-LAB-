c6dc75bea49540b7cb02bff4a1d5a7bbc0398994
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 08:56:30 +0800

    Update magkaibigan.php

--- TFA4/magkaibigan.php
+++ TFA4/magkaibigan.php
@@ -1,13 +1,14 @@
-<div class = "card">
-    <h1><b> Ang Magkaibigan </b></h1>
+<?php require ('header.php') ?>
 
-    <!-- image obtain from Pinterest -->
-    <img src="magkaibigan.jpg" alt="Avatar" style = "width:100%">
-    <div class="container">
-    <p>
+<div class="div">
+    <h1><b> Magkaibigan </b></h1>
+    <hr>
+    <p><h2>
         Isang araw, dumalo ang dalaga sa liga ng kanyang kaibigan. Hindi man ito nakaplano ay pinilit niyang magpunta dahil nangako siyang susuportahan ang binata. 
         Hindi niya akalain na ang simpleng pagdalo na iyon ang magiging simula ng isang pagkakaibigang unti-unting lumalalim.
         Matapos ang laro ay napagpasyahan nilang umuwi. Nauunang naglakad ang dalaga at pinagmamasdan lang siya ng binata na siyang nasa likuran ng dalaga. Tuluyang umaasa ang binata na sana'y mapansin siya ng dalaga. 
         Ngunit lingin sa kaalaman ng binata, unti unti na rin palang nahuhulog ang dalaga sakanya.
-    </p>
-</div>  
+    </p></h2>
+</div>
+
+<?php include ('footer.php') ?>
