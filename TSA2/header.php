ad15756f93d0f431f95a44112d7725d6c7ea32c0
maria <seemaallisongrace02@gmail.com>
2026-05-23 23:14:16 +0800

    Create header.php

---
+++ TSA2/header.php
@@ -0,0 +1,12 @@
+<header class = header>
+    <?php
+    $name = "Ma. Allison Grace N. See";
+    $title = "Cybersecurity Student";
+?>
+
+<div class="header">
+    <h1><?php echo $name; ?></h1>
+    <hr>
+    <p><?php echo strtoupper($title); ?></p>
+</div>
+</header>
