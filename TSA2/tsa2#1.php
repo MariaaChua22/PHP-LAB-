7ab51f0fcf2260acecebb8b87120fa4706f32353
maria <seemaallisongrace02@gmail.com>
2026-05-23 20:40:49 +0800

    Update tsa2#1.php

--- TSA2/tsa2#1.php
+++ TSA2/tsa2#1.php
@@ -6,125 +6,125 @@
 
 <body>
 
-<h1>Fruit Directory</h1>
-
 <?php
+    $fruits = array(
 
-$fruits = array(
+        array(
+            "no." => 1,
+            "Name" => "Avocado",
+            "Image" => "images/avocado.jpg",
+            "Description" => "A creamy green fruit.",
+            "Facts" => "Avocados are rich in healthy fats."
+        ),
 
-    array(
-        "no." => 1,
-        "Name" => "Apple",
-        "Image" => "images/apple.jpg",
-        "Description" => "A sweet and crunchy fruit.",
-        "Facts" => "Apples are rich in fiber and Vitamin C."
-    ),
+        array(
+            "no." => 2,
+            "Name" => "Blueberry",
+            "Image" => "images/blueberry.jpg",
+            "Description" => "A small blue-purple fruit.",
+            "Facts" => "Blueberries help improve brain function."
+        ),
 
-    array(
-        "no." => 2,
-        "Name" => "Banana",
-        "Image" => "images/banana.jpg",
-        "Description" => "A soft and sweet yellow fruit.",
-        "Facts" => "Bananas are rich in potassium."
-    ),
+        array(
+            "no." => 3,
+            "Name" => "Coconut",
+            "Image" => "images/coconut.jpg",
+            "Description" => "A tropical fruit with hard shell.",
+            "Facts" => "Coconut water helps hydrate the body."
+        ),
 
-    array(
-        "no." => 3,
-        "Name" => "Cherry",
-        "Image" => "images/cherry.jpg",
-        "Description" => "A small red juicy fruit.",
-        "Facts" => "Cherries contain antioxidants."
-    ),
+        array(
+            "no." => 4,
+            "Name" => "Dragon Fruit",
+            "Image" => "images/dragonfruit.jpg",
+            "Description" => "An exotic fruit with vibrant color.",
+            "Facts" => "Dragon fruit is high in antioxidants."
+        ),
 
-    array(
-        "no." => 4,
-        "Name" => "Grapes",
-        "Image" => "images/grapes.jpg",
-        "Description" => "A small round fruit that grows in bunches.",
-        "Facts" => "Grapes can be used to make wine."
-    ),
+        array(
+            "no." => 5,
+            "Name" => "Guava",
+            "Image" => "images/guava.jpg",
+            "Description" => "A sweet fruit with edible seeds.",
+            "Facts" => "Guava contains plenty of Vitamin C."
+        ),
 
-    array(
-        "no." => 5,
-        "Name" => "Kiwi",
-        "Image" => "images/kiwi.jpg",
-        "Description" => "A green fruit with tiny black seeds.",
-        "Facts" => "Kiwi contains more Vitamin C than oranges."
-    ),
+        array(
+            "no." => 6,
+            "Name" => "Lemon",
+            "Image" => "images/lemon.jpg",
+            "Description" => "A sour citrus fruit.",
+            "Facts" => "Lemons are commonly used for detox drinks."
+        ),
 
-    array(
-        "no." => 6,
-        "Name" => "Mango",
-        "Image" => "images/mango.jpg",
-        "Description" => "A juicy tropical fruit.",
-        "Facts" => "Mango is known as the national fruit of the Philippines."
-    ),
+        array(
+            "no." => 7,
+            "Name" => "Papaya",
+            "Image" => "images/papaya.jpg",
+            "Description" => "A soft orange tropical fruit.",
+            "Facts" => "Papaya contains enzymes that aid digestion."
+        ),
 
-    array(
-        "no." => 7,
-        "Name" => "Orange",
-        "Image" => "images/orange.jpg",
-        "Description" => "A citrus fruit rich in juice.",
-        "Facts" => "Oranges are high in Vitamin C."
-    ),
+        array(
+            "no." => 8,
+            "Name" => "Peach",
+            "Image" => "images/peach.jpg",
+            "Description" => "A juicy fruit with fuzzy skin.",
+            "Facts" => "Peaches are low in calories and high in fiber."
+        ),
 
-    array(
-        "no." => 8,
-        "Name" => "Pineapple",
-        "Image" => "images/pineapple.jpg",
-        "Description" => "A tropical fruit with rough skin.",
-        "Facts" => "Pineapple helps improve digestion."
-    ),
+        array(
+            "no." => 9,
+            "Name" => "Pear",
+            "Image" => "images/pear.jpg",
+            "Description" => "A sweet bell-shaped fruit.",
+            "Facts" => "Pears help improve heart health."
+        ),
 
-    array(
-        "no." => 9,
-        "Name" => "Strawberry",
-        "Image" => "images/strawberry.jpg",
-        "Description" => "A heart-shaped red fruit.",
-        "Facts" => "Strawberries are rich in antioxidants."
-    ),
+        array(
+            "no." => 10,
+            "Name" => "Rambutan",
+            "Image" => "images/rambutan.jpg",
+            "Description" => "A hairy tropical fruit.",
+            "Facts" => "Rambutan is rich in iron and Vitamin C."
+        )
 
-    array(
-        "no." => 10,
-        "Name" => "Watermelon",
-        "Image" => "images/watermelon.jpg",
-        "Description" => "A large fruit filled with water.",
-        "Facts" => "Watermelon helps keep the body hydrated."
-    )
+    );
 
-);
+    usort($fruits, function($a, $b) {
+        return strcasecmp($a['Name'], $b['Name']);
+    });
 
-usort($fruits, function($a, $b) {
-    return strcasecmp($a['Name'], $b['Name']);
-});
+    echo "<table border='2'>";
 
-echo "<table border='2'>";
+    echo "<tr>
+            <td colspan='5'><b>Fruit Directory</b></td>
+          </tr>";
 
-echo "<tr>
-        <th>No.</th>
-        <th>Name</th>
-        <th>Image</th>
-        <th>Description</th>
-        <th>Facts</th>
-      </tr>";
+    echo "<tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Description</th>
+            <th>Facts</th>
+          </tr>";
 
-$i = 1;
+    $i = 1;
 
-foreach($fruits as $row)
-{
-    echo "<tr>";
+    foreach($fruits as $row)
+    {
+        echo "<tr>";
 
-    echo "<td>" . $i++ . "</td>";
-    echo "<td>" . $row["Name"] . "</td>";
-    echo "<td><img src='" . $row["Image"] . "' width='120'></td>";
-    echo "<td>" . $row["Description"] . "</td>";
-    echo "<td>" . $row["Facts"] . "</td>";
+        echo "<td>" . $i++ . "</td>";
+        echo "<td>" . $row["Name"] . "</td>";
+        echo "<td><img src='" . $row["Image"] . "' width='120'></td>";
+        echo "<td>" . $row["Description"] . "</td>";
+        echo "<td>" . $row["Facts"] . "</td>";
 
-    echo "</tr>";
-}
+        echo "</tr>";
+    }
 
-echo "</table>";
-
+    echo "</table>";
 ?>
 
 </body>
