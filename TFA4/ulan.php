aabbc06f3dfef0d22ad5e08906b68287f9b14869
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 08:56:36 +0800

    Update ulan.php

--- TFA4/ulan.php
+++ TFA4/ulan.php
@@ -1,10 +1,13 @@
-<div class = "card">
+<?php require ('header.php') ?>
+
+<div class="div">
     <h1><b> Ulan </b></h1>
 
-    <!-- image obtain from Pinterest -->
-    <img src="ulan.jpg" alt="Image" width="500px" height="300px">
-    <p>
+    <hr>
+    <p><h2>
         Nang bumuhos ang ulan, sabay na sumilong ang magkaibigan sa iisang payong. 
         Tahimik nilang tinatahak ang kahabaan ng Espana, ngunit sa bawat pagpatak ng ulan, may damdaming unti-unting sumisibol na hindi nila mabanggit.
-    </p>
-</div>  
+    </p><h2>
+</div>
+
+<?php include ('footer.php') ?>
