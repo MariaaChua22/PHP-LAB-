c029633f2d81aec61a166ea90899bf96a80742a2
maria <seemaallisongrace02@gmail.com>
2026-05-23 23:14:08 +0800

    Create certification.php

---
+++ TSA2/certification.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Certification</title>
+    <link rel="stylesheet" href="tsa2-3.css">
+</head>
+
+<body>
+
+<?php include("header.php");?>
+
+<div class="content-box">
+    <h2>Certification</h2>
+    <hr>
+    <ul>
+        <li>Java Certified</li>
+    </ul>
+    <img src="images/java.cert.png" height = 100 alt="cert">
+</div>
+
+<?php include("footer.php"); ?>
+
+</body>
+</html>
