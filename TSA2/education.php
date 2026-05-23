7df3db45dcd8b0f2dffbf98fb7d2731a8f80f943
maria <seemaallisongrace02@gmail.com>
2026-05-23 23:14:10 +0800

    Create education.php

---
+++ TSA2/education.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title>Educational Attainment</title>
+    <link rel="stylesheet" href="tsa2-3.css">
+</head>
+
+<body>
+
+<?php include("header.php");?>
+
+<div class="content-box">
+    <h2>Educational Attainment</h2>
+    <hr>
+
+    <p>
+        FEU Institute of Technology<br>
+        Bachelor of Science in Information Technology Specialized in Cybersecurity<br>
+        2024 - ongoing
+        <br>
+        <img src="images/feutech.jpg" height="100">
+    </p>
+
+    <p>
+        University of the East - Manila<br>
+        Senior High School - ASTEM<br>
+        2022 - 2024
+        <br>
+        <img src="images/ue.jpg" height="100">
+    </p>
+</div>
+
+<?php include("footer.php"); ?>
+
+</body>
+</html>
