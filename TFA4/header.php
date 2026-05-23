2fbf9e9b449a4d24b1c39577bc10286ea7d4f07e
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-05-23 08:56:22 +0800

    Create header.php

---
+++ TFA4/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Short Stories</title>
+    <link rel="stylesheet" href="tfa4.css">
+</head>
+<header class="header">
+    <h1> Akda ni Maria </h1>
+    <hr>
+</header>
