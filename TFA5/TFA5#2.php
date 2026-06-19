dec82321a3942010f40fcc6b66dae7ac4d164f4a
MariaaChua22 <seemaallisongrace02@gmail.com>
2026-06-19 23:07:25 +0800

    Update TFA5#2.php

--- TFA5/TFA5#2.php
+++ TFA5/TFA5#2.php
@@ -3,8 +3,7 @@ setcookie("firstname", "Ma. Allison Grace", time() + 10);
 setcookie("middlename", "Natividad", time() + 20);
 setcookie("last_name", "See", time()+ 30);
 
-echo "<pre>
-</pre>";
+echo "<pre>";
 var_dump($_COOKIE);
 echo "</pre>";
 ?>
